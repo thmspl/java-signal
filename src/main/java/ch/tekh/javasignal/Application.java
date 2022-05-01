0b2e5cd96057e11d60dc61b3922bf38dda6aa69c
Thomas Pauli <mail@thomaspauli.ch>
2022-05-01 11:27:31 +0200

    Adds initial spring boot project.

---
+++ src/main/java/ch/tekh/javasignal/Application.java
@@ -0,0 +1,29 @@
+package ch.tekh.javasignal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * {@link Application}
+ *
+ * @author thomas pauli | tekh.ch
+ */
+@EnableAsync
+@EnableScheduling
+@EnableJpaRepositories
+@EntityScan(basePackages = Application.ENTITY_LOCATION)
+@SpringBootApplication(scanBasePackages = Application.BASE_PACKAGE_LOCATION)
+public class Application {
+
+	public static final String BASE_PACKAGE_LOCATION = "ch.tekh.javasignal";
+	public static final String ENTITY_LOCATION = "ch.tekh.javasignal.persistence.entity";
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
